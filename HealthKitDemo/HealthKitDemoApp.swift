84e01490e84e25e43a90e7698eafea7bdefcebfe
1-eyeddd <seisyuu43@icloud.com>
2023-03-14 16:24:48 +0900

    Initial Commit

---
+++ HealthKitDemo/HealthKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitDemoApp.swift
+//  HealthKitDemo
+//
+//  Created by Tsubata Seisyu on 2023/03/14.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
